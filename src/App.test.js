81c807715bc93130aa56ab3772ffc39acd58c443
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-10-21 15:56:04 +0100

    Add failing mapbox-gl import test

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,7 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('imports mapbox-gl', () => {
+  const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+})
